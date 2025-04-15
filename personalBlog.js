2a200ee7a21e76153f8d5d1646a80e4911cbec94
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-04-15 14:19:14 +0300

    Added theme toggle.

---
+++ personalBlog.js
@@ -0,0 +1,21 @@
+/* Theme toggle */
+document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('darkModeSwitch');
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const currentTheme = localStorage.getItem('bsTheme') || (prefersDarkScheme ? 'dark' : 'light');
+
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'dark';
+
+    switchElement.addEventListener('change', function () {
+        const newTheme = this.checked ? 'dark' : 'light';
+        let newText = "";
+        htmlElement.setAttribute('data-bs-theme', newTheme);
+        if(newTheme == "dark") {
+            newText = "Light Mode";
+        } else {newText = "Dark Mode";}
+        document.getElementById("darkModeSwitch-text").innerHTML = newText;
+        localStorage.setItem('bsTheme', newTheme);
+    });
+});
