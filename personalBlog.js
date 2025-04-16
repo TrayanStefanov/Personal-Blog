8aaa133f54db11ede8d1d265a1d253619ddb167c
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-04-16 10:51:18 +0300

    Back to top btn.

--- personalBlog.js
+++ personalBlog.js
@@ -1,21 +1,42 @@
+window.onscroll = function() {
+    scrollFunction();
+};
+
+let backToTop = document.getElementById("btn-back-to-top");
+
+/* Checks if we have scrolled */
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTop.style.display = "block";
+    } else {    
+        backToTop.style.display = "none";
+    }
+}
+
+backToTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
 /* Theme toggle */
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const htmlElement = document.documentElement;
-    const switchElement = document.getElementById('darkModeSwitch');
+    const switchElement = document.getElementById("darkModeSwitch");
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const currentTheme = localStorage.getItem('bsTheme') || (prefersDarkScheme ? 'dark' : 'light');
+    const currentTheme = localStorage.getItem("bsTheme") || (prefersDarkScheme ? "dark" : "light");
 
-    htmlElement.setAttribute('data-bs-theme', currentTheme);
-    switchElement.checked = currentTheme === 'dark';
+    htmlElement.setAttribute("data-bs-theme", currentTheme);
+    switchElement.checked = currentTheme === "dark";
 
-    switchElement.addEventListener('change', function () {
-        const newTheme = this.checked ? 'dark' : 'light';
+    switchElement.addEventListener("change", function (){
+        const newTheme = this.checked ? "dark" : "light";
         let newText = "";
-        htmlElement.setAttribute('data-bs-theme', newTheme);
+        htmlElement.setAttribute("data-bs-theme", newTheme);
         if(newTheme == "dark") {
             newText = "Light Mode";
         } else {newText = "Dark Mode";}
         document.getElementById("darkModeSwitch-text").innerHTML = newText;
-        localStorage.setItem('bsTheme', newTheme);
+        localStorage.setItem("bsTheme", newTheme);
     });
 });
